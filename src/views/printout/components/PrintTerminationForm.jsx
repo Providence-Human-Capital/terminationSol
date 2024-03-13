38187fad6aaa4524276eec7c8ee8ef3f3c6a120a
TafadzwaG <60160165+TafadzwaG@users.noreply.github.com>
2024-03-13 16:06:14 +0200

    upload csv

--- src/views/printout/components/PrintTerminationForm.jsx
+++ src/views/printout/components/PrintTerminationForm.jsx
@@ -57,6 +57,7 @@ const PrintTerminationForm = () => {
   const [isPrinting, setIsPrinting] = useState(false);
   const [examData, setExamData] = useState({});
   const [csvData, setCsvData] = useState([]);
+  const [columnArray, setColumnArray] = useState([]);
 
   const validationSchema = Yup.object().shape({
     company: Yup.string().required("Please  Enter The Company Name"),
@@ -94,7 +95,7 @@ const PrintTerminationForm = () => {
       },
     });
   };
-  const PrintCertificates = () => {
+  const PrintTerminationForms = () => {
     Swal.fire({
       icon: "warning",
       title: "Confirmation",
@@ -136,7 +137,7 @@ const PrintTerminationForm = () => {
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
         >
-          {({}) => (
+          {({ isSubmitting, setFieldValue, values }) => (
             <Form>
               <div className="card p-4 mt-5">
                 <div className="row g-3">
@@ -147,7 +148,7 @@ const PrintTerminationForm = () => {
                         fontWeight: "bold",
                       }}
                     >
-                      Printing Termination Certificate{" "}
+                      Printing Termination Forms{" "}
                     </h2>
                     <span className="text-muted">
                       Please make sure that you upload the csv files containing
@@ -167,11 +168,147 @@ const PrintTerminationForm = () => {
                       file{" "}
                     </h4>
                   </div>
+                  <div className="row">
+                    <div className="col-lg-4 col-md-12">
+                      <div className="form-floating">
+                        <Field
+                          type="text"
+                          className="form-control"
+                          id="company"
+                          name="company"
+                        />
+                        <label htmlFor="company">COMPANY</label>
+                        <ErrorMessage
+                          name="company"
+                          component="div"
+                          style={{
+                            color: "red",
+                          }}
+                          className="error-message"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-lg-6 col-md-12">
+                      <div className="form-floating">
+                        <input
+                          type="file"
+                          className="form-control"
+                          id="fileInput"
+                          name="fileInput"
+                          accept=".csv"
+                          onChange={(event) => {
+                            setFieldValue(
+                              "fileInput",
+                              event.currentTarget.files[0]
+                            );
+                          }}
+                        />
+                        <label for="fileInput">
+                          EMPLOYEE CSV (Make sure the file is a csv file)
+                        </label>
+                        <ErrorMessage
+                          name="fileInput"
+                          component="div"
+                          style={{
+                            color: "red",
+                          }}
+                          className="error-message"
+                        />
+                      </div>
+                    </div>
+                    <div className="space"></div>
+                    <div className="col-12 mt-4">
+                      <button
+                        className="btn btn-primary text-uppercase"
+                        disabled={isPrinting}
+                        style={{
+                          borderRadius: "10px",
+                        }}
+                        type="submit"
+                      >
+                        LOAD EMPLOYEES {"  "}
+                        <i className="fa fa-print"></i>
+                      </button>
+                    </div>
+                    <div className="space"></div>
+                    <h4>
+                      {" "}
+                      <span
+                        style={{
+                          fontWeight: "bold",
+                        }}
+                      >
+                        NB:
+                      </span>{" "}
+                      Note that you can only print the forms if you have
+                      successfully loaded the names{" "}
+                    </h4>
+                    <div>
+                      <button
+                        className="btn btn-primary text-uppercase"
+                        onClick={PrintTerminationForms}
+                        disabled={csvData.length <= 0}
+                        style={{
+                          borderRadius: "10px",
+                        }}
+                      >
+                        PRINT CERTIFICATES NOW
+                        <i className="fa fa-print"></i>
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </Form>
           )}
         </Formik>
+        <div className="box">
+          <div className="box-header no-border">
+            <h4
+              className="box-title"
+              style={{
+                textTransform: "uppercase",
+                fontWeight: "bold",
+              }}
+            >
+              Certificates Awaiting Printing
+            </h4>
+          </div>
+          <div className="box-body pt-0">
+            <div className="row mt-25">
+              <div className="table-responsive">
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th className="bb-2">DATE OF TERMINATION</th>
+                      <th className="bb-2">FIRST NAME</th>
+                      <th className="bb-2">SURNAME</th>
+                      <th className="bb-2">COMPANY</th>
+                      <th className="bb-2">ACTIONS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* date_of_termination */}
+                    {csvData &&
+                      csvData.map((client, index) => (
+                        <tr key={index}>
+                          <td>{client.date_of_termination}</td>
+                          <td>{client.first_name}</td>
+                          <td>{client.surname}</td>
+                          <td>{client.company}</td>
+                          <td>
+                            <button className="btn btn-primary" style={{
+                                borderRadius: "10px"
+                            }}>BCF PRINT</button>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
